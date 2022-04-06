55dde1dd9abfb41c33ff41ec14d788f3704c0e5f
judsonpaiva <judson.paiva@hotmail.com>
2022-04-07 00:17:28 +0100

    classes aninhadas

---
+++ src/Aninhamento.java
@@ -0,0 +1,38 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Aninhamento extends JFrame{
+	
+	JButton botao;
+	
+	public Aninhamento() {
+		super("Aninhamento");
+		
+		botao = new JButton("Ok");
+		botao.addActionListener(new ListenerAninhado());
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 300);
+		setVisible(true);
+	}
+	
+	
+	public class ListenerAninhado implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Aninhada: " +botao.getText());
+		}
+		
+	}
+	
+	
+	public static void main(String[] args)  {
+		
+		Aninhamento janela = new Aninhamento();
+
+	}
+
+}
